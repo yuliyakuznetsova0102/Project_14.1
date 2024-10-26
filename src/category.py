6f46fb4d46c043c96c49c4a517ec1ada9bc90b5a
Yuliya Kuznetsova <juliakuznetsova001@gmail.com>
2024-10-26 13:19:57 +0300

    hw15.1 done1

--- src/category.py
+++ src/category.py
@@ -22,7 +22,10 @@ class Category:
         print(Category.product_count)
 
     def __str__(self):
-        return f"{self.name}, количество продуктов: {len(self.__products)} шт."
+        all_quantity = 0
+        for j in self.__products:
+            all_quantity += j.quantity
+        return f"{self.name}, количество продуктов: {all_quantity} шт."
 
     def add_product(self, product: Product) -> Any:
         self.__products.append(product)
--- tests/test_category.py
+++ tests/test_category.py
@@ -30,5 +30,6 @@ def test_cat_get_product_list_property(first_category, second_category):
     )
 
 def test_category_str(first_category, second_category):
-    assert str(first_category) == "Category, количество продуктов: 2 шт."
-    assert str(second_category) == "Category number two, количество продуктов: 3 шт."
+    assert str(first_category) == "Category, количество продуктов: 44 шт."
+    assert str(second_category) == "Category number two, количество продуктов: 76 шт."
+
